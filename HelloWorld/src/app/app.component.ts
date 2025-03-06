66cdced1d66cede0bf4c100b99c4f76bc39938b8
Shamiksha <silkyraj23@gmail.com>
2025-03-07 00:56:22 +0530

    [shamiksha] add: ability to display hello from Bridgelabz with user input code added✨

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule }  from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl="Bridgelabzlogo.jpg";
+  userName: string = '';
 
   ngOnInit(): void {
     this.title="Hello from Bridgelabz!!!! ";
